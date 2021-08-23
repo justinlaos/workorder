a6e767f407597ab613858cf53dec3674c555be27
Justin Laos <justinlaos@Justins-MacBook-Pro.local>
2021-08-22 18:07:37 -0700

    set seeding for properties data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach(Rails.root.join('lib/properties_seeds.csv'), headers: true) do |row|
+  Property.create( {
+    property_type: row["PROPERTY_TYPE"],
+    address: row["ADDRESS"], 
+    city: row["CITY"], 
+    state: row["STATE_OR_PROVINCE"], 
+    zip_code: row["ZIP_OR_POSTAL_CODE"], 
+    price: row["PRICE"], 
+    bed_rooms: row["BEDS"], 
+    bath_rooms:row["BATHS"], 
+    square_feet: row["SQUARE_FEET"], 
+    lot_size: row["LOT_SIZE"], 
+    year_built: row["YEAR_BUILT"], 
+    latitude: row["LATITUDE"], 
+    longitude: row["LONGITUDE"]
+  } ) 
+end
